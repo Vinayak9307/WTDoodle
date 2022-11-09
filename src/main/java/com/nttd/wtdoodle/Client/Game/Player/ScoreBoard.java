e49adab33923bde561ba1e2ab9bd387e22df883f
Backlit <saket9max@gmail.com>
2022-11-09 18:58:16 +0530

    added scoreboard

---
+++ src/main/java/com/nttd/wtdoodle/Client/Game/Player/ScoreBoard.java
@@ -0,0 +1,60 @@
+package com.nttd.wtdoodle.Client.Game.Player;
+
+import com.nttd.wtdoodle.ResourceLocator;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+import javafx.scene.text.Font;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ScoreBoard extends Application implements Initializable {
+    public Button btn_goToDashboard;
+    public GridPane gridPane;
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+     btn_goToDashboard.setOnAction(new EventHandler<ActionEvent>() {
+         @Override
+         public void handle(ActionEvent event) {
+             FXMLLoader fxmlLoader = new FXMLLoader(ResourceLocator.class.getResource("Dashboard.fxml"));
+             Stage stage=(Stage)((Node)event.getSource()).getScene().getWindow();
+
+             Scene scene = null;
+             try {
+                 scene = new Scene(fxmlLoader.load(), 950, 570);
+             } catch (IOException e) {
+                 throw new RuntimeException(e);
+             }
+
+             stage.setScene(scene);
+             stage.show();
+         }
+     });
+
+     /*populate the grid with score of player in the game*/
+        int i=1;
+        gridPane.addRow(1,createLabel("Rank"),createLabel("Username"),createLabel("Score"));
+
+    }
+    private Label createLabel(String s){
+        Label label=new Label(s);
+        label.setFont(Font.font("Verdana",15));
+        return label;
+    }
+}
